68da48a4f8ceb2ab87b20c2b563c8bfd631eaa91
yakenohara <38835246+yakenohara@users.noreply.github.com>
2018-05-06 22:44:59 +0900

    ExplorerメニューのSendToから実行可能した

---
+++ Touch.bat
@@ -0,0 +1,26 @@
+@echo off
+
+::定数
+set ps1FileName=Touch.ps1
+
+::初期化
+set ps1FileFullPath=%~dp0%ps1FileName%
+
+
+::引数生成ループ
+set param=
+for %%f in (%*) do (
+  call :concat %%f
+)
+
+::Call powershell
+powershell "%ps1FileFullPath%%param%"
+
+exit /b
+
+
+::引数生成サブルーチン
+:concat
+set param=%param% \"%~1\"
+
+exit /b
