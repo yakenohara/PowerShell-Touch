f6a43e501519ce5744b60e43ce72f7d815c48868
yakenohara <38835246+yakenohara@users.noreply.github.com>
2018-07-19 19:38:08 +0900

    add a license

--- Touch.bat
+++ Touch.bat
@@ -1,3 +1,22 @@
+::<License>------------------------------------------------------------
+::
+:: Copyright (c) 2018 Shinnosuke Yakenohara
+::
+:: This program is free software: you can redistribute it and/or modify
+:: it under the terms of the GNU General Public License as published by
+:: the Free Software Foundation, either version 3 of the License, or
+:: (at your option) any later version.
+::
+:: This program is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+:: GNU General Public License for more details.
+::
+:: You should have received a copy of the GNU General Public License
+:: along with this program.  If not, see <http://www.gnu.org/licenses/>.
+::
+::-----------------------------------------------------------</License>
+
 @echo off
 
 ::定数
@@ -14,7 +33,7 @@ for %%f in (%*) do (
 )
 
 ::Call powershell
-powershell "& \"%ps1FileFullPath%\" /r%param%"
+powershell -ExecutionPolicy Unrestricted "& \"%ps1FileFullPath%\" /r%param%"
 
 exit /b
 
